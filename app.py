da75facfd1a4dd13e1632c9917a1c3f1a52bdfff
Jorge García-Torres <36573528+JTorres258@users.noreply.github.com>
2025-08-27 12:29:28 +0000

    Use T5-base

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ os.environ["TRANSFORMERS_NO_PYTORCH"] = "1"
 from transformers import pipeline
 import gradio as gr
 
-model = pipeline("summarization")
+model = pipeline("summarization", model="t5-base", tokenizer="t5-base", framework="tf")
 
 def predict(prompt):
     summary = model(prompt)[0]["summary_text"]
